ae6eac51f41d2510fb7367ad41605f5071cebfaa
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2017-07-31 09:39:18 -0400

    added setup scripts

---
+++ setup.py
@@ -0,0 +1,38 @@
+
+
+import os
+import sys
+
+assert "ASCDS_INSTALL" in os.environ, "Please run this after CIAO has been setup"
+
+#
+# I need the pythonX.Y version for the purelib's.  Can't find a way 
+# to pass in so that it gets picked up by setup.cfg so we just set
+# and env variable and that works.
+#
+
+ver = sys.version_info
+os.environ["PYVER"] = "python{}.{}".format(ver[0],ver[1])
+
+###
+### distutils replaces the shebang /usr/bin/env python with sys.executable.
+### This doesn't work with CIAO where we need to run the wrapper  in 
+### $ASCDS_INSTALL/bin instead of $ASCDS_INSTALL/ots/bin
+###
+### So, we change it before running setup()
+
+sys.executable = os.path.join( os.environ["ASCDS_INSTALL"], "bin", "python"  )
+
+
+
+from distutils.core import setup
+setup( name='TemperatureMaps',
+        version='0.0.1',
+        description='Temperature maps scripts',
+        author='Anonymous',
+        author_email='WhoDat@cfa.harvard.edu',
+        url='https://github.com/kglotfelty/TemperatureMaps/',
+        scripts = ["contour_bin"],
+        data_files = ["contour_bin.par"] 
+        )
+
