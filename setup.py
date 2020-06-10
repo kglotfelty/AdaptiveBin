d86bacca55394182b2577b78ad45c5dd68f32196
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2020-06-10 09:01:59 -0400

    force masked_image_crate into crates_contrib

--- setup.py
+++ setup.py
@@ -5,15 +5,6 @@ import sys
 
 assert "ASCDS_INSTALL" in os.environ, "Please run this after CIAO has been setup"
 
-#
-# I need the pythonX.Y version for the purelib's.  Can't find a way 
-# to pass in so that it gets picked up by setup.cfg so we just set
-# and env variable and that works.
-#
-
-ver = sys.version_info
-os.environ["PYVER"] = "python{}.{}".format(ver[0],ver[1])
-
 
 scripts = ["contour_map","dragon_scales","vtbin","hexgrid","mkregmap","pathfinder",
             "centroid_map", "merge_too_small", "map2reg"]
@@ -30,6 +21,6 @@ setup( name='AdaptiveBin',
         url='https://github.com/kglotfelty/AdaptiveBin/',
         scripts = scripts,
         data_files = [ ("param", params ), ("share/doc/xml", docs ) ],
-        py_modules=["masked_image_crate"]        
+        packages=["crates_contrib",]        
         )
 
